31365711243a91086498d110586307c71a1a09c5
cb <216@gmail.com>
2023-04-29 16:35:46 +0800

    初次尝试使用v3+ts

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Son from './components/Son.vue'
+const money = ref(1000)
+const car = ref('宝马')
+</script>
+
+<template>
+  <div>
+    {{ money }} -- {{ car }}
+    <Son :money="money"></Son>
+  </div>
+</template>
---
+++ src/components/Son.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+// defineProps({
+//   money: {
+//     type: Number,
+//     default: 5000
+//   },
+//   car: {
+//     type: String,
+//     default: '迈凯伦'
+//   }
+// })
+
+/*
+TS 的写法
+  1. 调用 defineProps ，把泛型参数(描述父组件的props的类型) 传递，返回数据
+  2. 解构返回来的数据，并且加上默认值
+    注意：要使用这个功能，要在 vite.config.ts 里面开启
+    vue({
+        reactivityTransform: true,
+    })
+    并且开启后，需要重启服务
+    开启后，vscode黄色的警告不用管，是提醒你注意版本号的
+*/
+
+// ts泛型写法  可以利用解构赋值去赋予默认值
+const { money = 1000, car = '迈凯伦' } = defineProps<{
+  money: number
+  car?: string // 非必传
+}>()
+</script>
+<template>
+  <div>{{ money }}---{{ car }}</div>
+</template>
